8065848dd23e642fbbb10c8783a4e2da6903605b
Rob Lingle <rob@actsasif.com>
2009-04-07 05:03:04 -0400

    rearranging output

--- weasel_cli.rb
+++ weasel_cli.rb
@@ -1,14 +1,26 @@
 require File.join(File.dirname(__FILE__), 'lib', 'weasel.rb')
 
-@weasel = Weasel.new
+options = {
+  :target               => "Methinks it is like a weasel",
+  :offspring            => 100,
+  :length_penalty       => 5,
+  :spelling_multiplier  => 5,
+  :spelling_mutations   => 2,
+  :length_mutations     => 2,
+  :allow_clones         => true
+}
+
+@weasel = Weasel.new("protozoa",options)
 puts @weasel.selected
 while @weasel.selected.fitness != 0 do
   @weasel.generation
+  puts "Current: " + @weasel.selected
+  puts "Score: #{@weasel.selected.fitness}"
   puts "Generation: #{@weasel.selected.generation}"
   puts "Parent: " + @weasel.selected.parent
-  puts "Current: " + @weasel.selected
   puts 
   puts "Random: #{@weasel.random}"
+  puts "Score: #{@weasel.random.fitness}"
   puts
 end
   
